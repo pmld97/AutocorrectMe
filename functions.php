3cd8f3725a48b00e5295396bd45f336fce2a0164
pmld97 <pmldavidsen@gmail.com>
2019-11-19 10:18:46 +0100

    Added comments to source code

--- functions.php
+++ functions.php
@@ -9,15 +9,15 @@
 *
 */
 
-defined( 'ABSPATH' ) or die( 'You are not supposed to be here, move along!' );
+defined( 'ABSPATH' ) or die( 'Stop right there you criminal scum! >:(' ); //Don't hack me plz
 
-add_filter('the_content', array('filterhook_fix_gramma', 'fix_spelling'));
-add_action('wp_enqueue_scripts', array('filterhook_fix_gramma', 'includes'));
+add_filter('the_content', array('filterhook_fix_gramma', 'fix_spelling')); //Hook and using a callback-function
+add_action('wp_enqueue_scripts', array('filterhook_fix_gramma', 'includes')); //Telling the system I want to include my costum css in a function
 
-class filterhook_fix_gramma{
-        function fix_spelling($content)
+class filterhook_fix_gramma{ //The hook
+        function fix_spelling($content) //The actual callback function
         {
-            $search = array('its', 'thats', 'ive', 'im', 'wouldnt', 'werent');
+            $search = array('its', 'thats', 'ive', 'im', 'wouldnt', 'werent', 'Autocorrect'); //Searching for the typed in words in content
 
             $replace = array(
                 "<a class='corrected'>" . "it's" . "</a>",
@@ -26,11 +26,12 @@ class filterhook_fix_gramma{
                 "<a class='corrected'>" . "I'm" . "</a>",
                 "<a class='corrected'>" . "wouldn't" . "</a>",
                 "<a class='corrected'>" . "weren't" . "</a>",
-            );
+                "<a class='corrected'>" . "autocorrect" . "</a>",
+            ); //Correcting the words
 
-           return str_replace($search, $replace, $content);
+           return str_replace($search, $replace, $content); //Returning the correction before the content is displayed
         }
-        function includes (){
-            wp_enqueue_style ("style", plugins_url() . "/AutocorrectMe/style.css");
+        function includes (){ 
+            wp_enqueue_style ("style", plugins_url() . "/AutocorrectMe/style.css"); //Actually including the css
         }
 }
